fa9f3d19b817d2c03245876cfff9c62d5060edda
0lga <otoiker@yandex.ru>
2022-12-04 16:25:18 +0300

    DZ2_seminar2

---
+++ DZ1Chislo987Pokazat8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// трехзначное число и на выходе показывает
+// вторую цифру этого числа. 
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int a1 = num / 100;
+int a2 = num % 10;
+int result = (num - a1 * 100 - a2) / 10;
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine($"{num} -> {result}");
+}
+else 
+    if (num < -99 && num > -1000)
+    {
+        Console.WriteLine($"{num} -> {-result}");
+    }
+else Console.WriteLine($"Вы ввели не трехзначное число!!!");
---
+++ DZ2TretiaCyfra/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = num / 100;
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine($"Третья цифра числа {num} -> {result}");
+}
+else if (num < -99 && num > -1000)
+{
+Console.WriteLine($"Третья цифра числа {num} -> {-result}");    
+}
+
+else if (num > 999)
+{
+    result = num%1000/100;
+    Console.WriteLine($"Третья цифра числа {num} -> {result}");
+}
+else if (num < 999)
+{
+    result = num % 1000 / 100;
+    Console.WriteLine($"Третья цифра числа {num} -> {-result}");
+}
+else Console.WriteLine($"У заданного числа нет третьей цифры!");
